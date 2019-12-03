b4c43fcc876516edfc6357c012e99c13af5c3441
Jura Prokofev <Smyrke2005@yandex.ru>
2019-12-03 17:17:21 +0300

    Update myproto.cpp and myproto.h

--- myproto.h
+++ myproto.h
@@ -4,68 +4,68 @@
 #include <QByteArray>
 #include <list>
 
-enum{
-	param_type_empty,
-	param_type_helo,
-	param_type_name,
-	param_type_where_are_you,
-	param_type_i_am_here,
-	param_type_bye,
-};
-
-enum{
-	pkt_type_helo,
-	pkt_type_bye,
-};
-
-enum{
-	pkt_channel_comunication		= 1,
-	pkt_channel_auth,
-};
-
-struct PktData
-{
-	uint16_t type = param_type_empty;
-	uint16_t size = 0;
-	QByteArray data;
-};
-
 /*
  * Packet Structure
  *
  * preamble					4 byte
- * channelNum				1 byte
+ * channel					1 byte
+ * source					4 byte
  * destination				4 byte		if destination = 0xFFFFFFFF then this message from all
  * dataLength				4 byte
  * format/encrypt data		(dataLength) bytes
- * crc						2 byte
+ * crc						4 byte
  * -------------------------------------------------------
- * total header				13 bytes
- * total packet				15 + (dataLength) bytes
+ * total header				17 bytes
+ * total packet				21 + (dataLength) bytes
 */
 
-struct Pkt{
-	uint32_t preamble				= 0;
-	uint8_t chanelNum				= 0;
-	uint32_t destination			= 0;
-	uint32_t dataLength				= 0;
-	QByteArray rawData;
-	uint16_t crc					= 0;
-	std::list<PktData> pktData;
-	const uint8_t headerSize		= 13;
-	bool next = false;
-	bool error = false;
-	bool retry = false;
-};
 
 namespace myproto {
+	struct Channel{
+		enum{
+			comunication				= 1,
+			auth,
+		};
+	};
+	struct PktType{
+		enum{
+			hello,
+			bye,
+		};
+	};
+	struct DataType{
+		enum{
+			empty,
+			text,
+			name,
+		};
+	};
+	struct PktData {
+		uint16_t type					= myproto::DataType::empty;
+		uint16_t size					= 0;
+		QByteArray data;
+	};
+	struct Pkt{
+		uint32_t preamble				= 0;
+		uint8_t channel					= 0;
+		uint32_t source					= 0;
+		uint32_t destination			= 0;
+		uint32_t dataLength				= 0;
+		QByteArray rawData;
+		uint16_t crc					= 0;
+		std::list<myproto::PktData> pktData;
+		const uint8_t headerSize		= 18;
+		bool next = false;
+		bool error = false;
+		bool retry = false;
+	};
 
 	Pkt parsPkt( QByteArray &data );
 	QByteArray buidPkt( const Pkt &pkt );
-	void parsParams( Pkt &pkt );
-	void setParam( QByteArray &data, const uint16_t param, const QByteArray &value = QByteArray() );
-	uint16_t getCRC( const QByteArray &data );
-	Pkt getPkt( const uint8_t chanelNum, const uint8_t type );
+	void parsData( Pkt &pkt );
+	void addData( QByteArray &data, const uint16_t param, const QByteArray &value = QByteArray() );
+	uint32_t getCRC( const QByteArray &data );
+	Pkt getPkt(const uint8_t type );
 }
 
 #endif // MYPROTO_H
